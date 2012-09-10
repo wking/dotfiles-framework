75a3866dda4400567ff3be3ec116ad3aa6f7ed4d
W. Trevor King <wking@tremily.us>
2012-09-10 13:26:14 -0400

    Add COPYING and include copyright blurb in dotfiles.sh (GPLv3+).

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,6 +2,21 @@
 #
 # Dotfiles management script.  For details, run
 #   $ dotfiles.sh --help
+#
+# Copyright (C) 2011-2012 W. Trevor King <wking@tremily.us>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 VERSION='0.2'
 DOTFILES_DIR="${PWD}"
